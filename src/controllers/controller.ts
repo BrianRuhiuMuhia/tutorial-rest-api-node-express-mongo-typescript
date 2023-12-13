9ba355a53f3faec7ff230c0cd2254a627b3e2a87
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2023-12-13 11:58:45 +0300

    middlewares

---
+++ src/controllers/controller.ts
@@ -0,0 +1,8 @@
+const UserModel=require("../models/model.ts")
+export const getUsers = () => UserModel.find();
+export const getUserByEmail = (email: string) => UserModel.findOne({ email });
+export const getUserBySessionToken = (sessionToken: string) => UserModel.findOne({ 'authentication.sessionToken': sessionToken });
+export const getUserById = (id: string) => UserModel.findById(id);
+export const createUser = (values: Record<string, any>) => new UserModel(values).save().then((user:string) => user);
+export const deleteUserById = (id: string) => UserModel.findOneAndDelete({ _id: id });
+export const updateUserById = (id: string, values: Record<string, any>) => UserModel.findByIdAndUpdate(id, values);
